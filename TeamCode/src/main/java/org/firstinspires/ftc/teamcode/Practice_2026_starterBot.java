2e695b4f01edcf7dc71deb215c55e01e7a9c9831
BothAchiever647 <169114233+BothAchiever647@users.noreply.github.com>
2025-09-18 19:28:50 -0600

    Added Base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_2026_starterBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_2026_starterBot.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Practice_2026_starterBot extends OpMode {
+
+    DcMotor LeftMotor;
 
     @Override
     public void init() {
