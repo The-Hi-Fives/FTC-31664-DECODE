af8029322ef0c1b8fd3a32ac72b562c4a5b8f2d4
BothAchiever647 <169114233+BothAchiever647@users.noreply.github.com>
2025-09-18 19:46:41 -0600

    Defined Motors/Servos.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_2026_starterBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_2026_starterBot.java
@@ -1,15 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
+import java.lang.Math;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Practice_2026_starterBot extends OpMode {
 
     DcMotor LeftMotor;
+    DcMotor RightMotor;
+    DcMotorEx Launcher;
+    Servo LeftFeeder;
+    Servo RightFeeder;
 
     @Override
     public void init() {
-
+        LeftMotor = hardwareMap.get(DcMotor.class,"leftMotor");
+        RightMotor = hardwareMap.get(DcMotor.class,"rightMotor");
+        Launcher = hardwareMap.get(DcMotorEx.class,"launchMotor");
+        LeftFeeder = hardwareMap.get(Servo.class,"left_feeder");
+        RightFeeder = hardwareMap.get(Servo.class,"right_feeder");
     }
 
     @Override
