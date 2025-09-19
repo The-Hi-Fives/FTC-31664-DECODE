1dd42cefeabd18ca0e84b59ec666540f6e35ede8
BothAchiever647 <169114233+BothAchiever647@users.noreply.github.com>
2025-09-18 19:23:11 -0600

    Added Base

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_2026_starterBot.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class Practice_2026_starterBot extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
