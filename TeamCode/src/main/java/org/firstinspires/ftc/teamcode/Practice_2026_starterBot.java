47533b6d89964cb4e5ef28bb828e540cf2c09a06
BothAchiever647 <169114233+BothAchiever647@users.noreply.github.com>
2025-09-20 09:26:54 -0600

    Added @TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_2026_starterBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice_2026_starterBot.java
@@ -29,6 +29,7 @@ public class Practice_2026_starterBot extends OpMode {
 
     @Override
     public void loop() {
+        //Test
         if (gamepad1.right_trigger > 0.6) {
             LeftFeeder.setPosition(300);
             RightFeeder.setPosition(300);
