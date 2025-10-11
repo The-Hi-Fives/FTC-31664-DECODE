3ad4dec2c5508e98e6212eff6ec1274b3bf54d6b
BothAchiever647 <169114233+BothAchiever647@users.noreply.github.com>
2025-10-11 10:57:48 -0600

    Added huskylens_apriltag_test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskylens_apriltag_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskylens_apriltag_test.java
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import java.util.Arrays;
 import java.util.List;
 
-
+@TeleOp
 public class huskylens_apriltag_test extends OpMode {
     HuskyLens Camera;
 
@@ -24,8 +25,8 @@ public class huskylens_apriltag_test extends OpMode {
         List<HuskyLens.Block> blocks = Arrays.asList(Camera.blocks());
         for (HuskyLens.Block block : blocks) {
             telemetry.addData("AprilTag ID:",block.id);
-            telemetry.addData("AprilTag 1 detected at X:", block.x);
-            telemetry.addData("AprilTag 1 detected at Y:", block.y);
+            telemetry.addData("AprilTag detected at X:", block.x);
+            telemetry.addData("AprilTag detected at Y:", block.y);
         }
         telemetry.update();
     }
