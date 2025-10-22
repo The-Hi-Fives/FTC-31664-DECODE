f2b96a854aadcf662363d73998d79b06876e9def
BothAchiever647 <169114233+BothAchiever647@users.noreply.github.com>
2025-10-21 19:46:25 -0600

    Changed Motor RunMode to RUN_USING_ENCODER, Added GetTag() Function, Added Telemetry and Controls Clarity.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Macanum_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Macanum_Test.java
@@ -3,16 +3,18 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import java.util.Arrays;
+import java.util.List;
 
 @TeleOp
 public class Macanum_Test extends OpMode {
-
-    DcMotor FrontLeftMotor;
-    DcMotor BackLeftMotor;
-    DcMotor FrontRightMotor;
-    DcMotor BackRightMotor;
+    DcMotorEx FrontLeftMotor;
+    DcMotorEx BackLeftMotor;
+    DcMotorEx FrontRightMotor;
+    DcMotorEx BackRightMotor;
     HuskyLens Camera;
 
     @Override
@@ -20,22 +22,46 @@ public class Macanum_Test extends OpMode {
         Camera = hardwareMap.get(HuskyLens.class,"Huskylens");
         Camera.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
 
-        FrontLeftMotor = hardwareMap.get(DcMotor.class,"frontLeft");
-        BackLeftMotor = hardwareMap.get(DcMotor.class,"backLeft");
-        FrontRightMotor = hardwareMap.get(DcMotor.class,"frontRight");
-        BackRightMotor = hardwareMap.get(DcMotor.class,"backRight");
+        FrontLeftMotor = hardwareMap.get(DcMotorEx.class,"frontLeft");
+        BackLeftMotor = hardwareMap.get(DcMotorEx.class,"backLeft");
+        FrontRightMotor = hardwareMap.get(DcMotorEx.class,"frontRight");
+        BackRightMotor = hardwareMap.get(DcMotorEx.class,"backRight");
+
+        FrontRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        BackRightMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        FrontLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        BackLeftMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
         FrontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         BackRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
-    public int getTag() {
-        for (int i = 0; i > 2; i++) {
-
+    public HuskyLens.Block getTag() {
+        List<HuskyLens.Block> blocks = Arrays.asList(Camera.blocks());
+        HuskyLens.Block targetBlock = null;
+        for (HuskyLens.Block block : blocks) {
+            if (block.id != 0) {
+                if (targetBlock != null) {
+                    if (block.x * block.y > targetBlock.x * targetBlock.y) {
+                        targetBlock = block;
+                    }
+                } else {
+                    targetBlock = block;
+                }
+            }
         }
 
-
-        return 0;
+        return targetBlock;
+    }
+    public void Telemetry(HuskyLens.Block block) {
+        telemetry.addLine("============");
+        telemetry.addLine("Controls:");
+        telemetry.addLine("Left Stick: Crab Movement");
+        telemetry.addLine("Right Stick: Rotation");
+        telemetry.addLine("============");
+        if (block != null) {
+            telemetry.addData("id:", block);
+        }
     }
 
 
@@ -53,9 +79,13 @@ public class Macanum_Test extends OpMode {
 
         double d = Math.max(Math.abs(x)+Math.abs(y)+Math.abs(r),1)/m;
 
-        FrontLeftMotor.setPower((x+y+r)/d);
-        BackLeftMotor.setPower((x-y+r)/d);
-        FrontRightMotor.setPower((x-y-r)/d);
-        BackRightMotor.setPower((x+y-r)/d);
+        FrontLeftMotor.setVelocity((x+y+r)/d*2000);
+        BackLeftMotor.setVelocity((x-y+r)/d*2000);
+        FrontRightMotor.setVelocity((x-y-r)/d*2000);
+        BackRightMotor.setVelocity((x+y-r)/d*2000);
+
+        HuskyLens.Block block = getTag();
+
+        Telemetry(block);
     }
 }
